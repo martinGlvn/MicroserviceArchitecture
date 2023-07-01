61c43452b42d1679619cf3b934ee65ec4c0130c6
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-01 15:01:45 -0300

    auth

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -12,6 +12,7 @@ server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
+#
 @server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
@@ -23,7 +24,6 @@ def login():
     res = cur.execute(
         "SELECT email, password FROM user WHERE email=%s", (auth.username,)
     )
-
     if res > 0:
         user_row = cur.fetchone()
         email = user_row[0]
@@ -35,3 +35,41 @@ def login():
             return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
         return "invalide credentials", 401
+    
+
+#
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+#
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+#
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
